f18c8f74c2a519e8f51c55a88aea21a9ecb21972
khusbaga <khusbaga@KHUSBAGA-LAP.oradev.oraclecorp.com>
2019-01-05 20:48:46 +0530

    My first ever commit

---
+++ demoApp/src/main/java/com/demo/demoApp/demoApp.java
@@ -0,0 +1,25 @@
+package com.demo.demoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoApp {
+
+	@RequestMapping("/first")
+	@GetMapping
+	public String returnValue() {
+		return "Hello Everyone !";
+	
+	}
+	
+	@RequestMapping("/hello")
+	@GetMapping
+	public String returnHello() {
+		return "Hey Buddy !";
+	
+	}
+	
+	
+}
